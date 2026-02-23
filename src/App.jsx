3a51ac8ee2c7e5c307f5a601e6cb2f5b2d5ed03b
Dev621-tech <devinosborne11@yahoo.com>
2026-02-23 15:53:59 -0500

    added useEffect & Try/Catch

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import './App.css'
 
 import MovieDisplay from './Components/MovieDisplay.jsx';
@@ -5,11 +6,39 @@ import Form from './Components/Form.jsx';
 
 function App() {
   
+  const apiKey = "98e3fb1f";
+
+  const [movie, setMovie] = useState(null);
+
+   // Function to get movies
+  const getMovie = async(searchTerm) => {
+
+    try {
+        // Make fetch request and store the response
+    const response = await fetch(
+      `http://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`
+    );
+    // Parse JSON response into a JavaScript object
+    const data = await response.json();
+    // Set the Movie state to the received data
+    setMovie(data);
+
+    } catch (error) {
+      console.error(error)
+    }
+  };
+  
+
+  // This will run on the first render but not on subsquent renders
+  useEffect(() => {
+    getMovie("Clueless");
+  }, []);
+
 
   return (
     <>
-      <Form />
-      <MovieDisplay />
+      <Form moviesearch={getMovie}/>
+      <MovieDisplay movie={movie} />
     </>
   )
 }
